be6e42ad965cf2b02bcf57fb33428aff0b252ed2
wed150 <wed15058338634@outlook.com>
2026-05-23 04:06:21 +0800

    fix: config error

--- js/utils.umd.js
+++ js/utils.umd.js
@@ -138,7 +138,7 @@ const i18n = {
         
         // Upload section
         SECTION_UPLOAD: 'Upload Files',
-        FORMAT_HINT: 'Supported: .mcstructure | .nbt | .litematic',
+        FORMAT_HINT: '.mcstructure|nbt|litematic',
         
         // Config section
         SECTION_CONFIG: 'Configuration',
@@ -181,7 +181,7 @@ const i18n = {
 };
 
 // 当前语言设置（可通过 switchLang 切换）
-let currentLang = 'zh';
+let currentLang = 'en';
 
 /**
  * 带变量替换的翻译函数
